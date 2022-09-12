fb2483854f49684a851f2f9e503239aef73f3d5e
mahmoud-adel-ahmed <mahmoudalghareeb253@gmail.com>
2022-09-12 15:51:14 +0200

    Freaky Template modified

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let bars = document.querySelector(".fa-bars");
 let nav = document.querySelector(".nav");
 let header = document.querySelector("header");
+let bullets = document.querySelectorAll(".bullets span");
+let img = document.querySelector(".company img");
 
 bars.addEventListener("click", () => {
   bars.classList.toggle("fa-times");
@@ -11,9 +13,6 @@ window.onscroll = function () {
   bars.classList.remove("fa-times");
   nav.classList.remove("active");
 };
-
-let bullets = document.querySelectorAll(".bullets span");
-let img = document.querySelector(".company img");
 
 bullets.forEach((bullet) => {
   bullet.addEventListener("click", () => {
